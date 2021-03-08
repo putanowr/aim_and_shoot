6170f2b51ddc2f6c43dcef77fecb2a74f31eef9f
Roman Putanowicz <putanowr@gmail.com>
2021-03-08 14:35:24 +0100

    Added comments explaining code fragments

--- src/projectile_alpha.m
+++ src/projectile_alpha.m
@@ -1,21 +1,30 @@
+% Simpe game based on projectile motion in 2D
 clear all
+
+% Initial condition
 v = 10;
 alpha = 40;
 x = 0; 
 y = 0;
 
+dt = 0.05;
+g = 9.81;
+
+% Map alha from radians to degree
 alpha = alpha * pi/180;
+
+% Calculate velocity components
 
 vx = v * cos(alpha);
 vy = v * sin(alpha);
 
-dt = 0.05;
-g = 9.81;
+% Initialize array for stroing velocity and position components
 xt(1) = x;
 yt(1) = y;
 vxt(1) = vx;
 vyt(1) = vy;
 
+% Iterate over time steps
 for i = 2 : 200
     vxt(i) = vxt(i-1);
     vyt(i) = vyt(i-1) - g*dt;
@@ -26,4 +35,5 @@ for i = 2 : 200
     end
 end
 
+% Visualise projectile trajectory
 plot(xt, yt, '*-b');    
